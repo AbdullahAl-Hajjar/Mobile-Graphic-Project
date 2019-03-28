a8e3d35611073a7b88ab93fb4844f6332841d8a5
nhall <hall0433@algonquinlive.com>
2019-03-28 08:53:10 -0400

    Toast added

--- app/src/main/java/com/example/androidfinalgroupproject/Dictionary.java
+++ app/src/main/java/com/example/androidfinalgroupproject/Dictionary.java
@@ -1,10 +1,13 @@
 package com.example.androidfinalgroupproject;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Dictionary extends AppCompatActivity {
 
@@ -15,12 +18,26 @@ public class Dictionary extends AppCompatActivity {
         Toolbar tBar = (Toolbar)findViewById(R.id.my_toolbar);
         tBar.setTitle(R.string.dictionaryTitle);
         setSupportActionBar(tBar);
+        //tBar.setTitleTextColor(getResources().getColor(R.color.colorWhite));
+        tBar.setTitleTextColor(ContextCompat.getColor(this, R.color.colorWhite));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.dictionary_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this,R.string.noSavedWords, Toast.LENGTH_LONG).show();
+                break;
+            case R.id.item2:
+                break;
+        }
         return true;
     }
 }
