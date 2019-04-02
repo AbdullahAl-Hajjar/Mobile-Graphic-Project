bb7a4f0906c0fd3cae4eb4879130a6b4caa2d631
nhall <hall0433@algonquinlive.com>
2019-04-02 02:46:24 -0400

    Async task added, search functional, headword and pronunciation fields implemented.

--- app/src/main/java/com/example/androidfinalgroupproject/Dictionary.java
+++ app/src/main/java/com/example/androidfinalgroupproject/Dictionary.java
@@ -1,20 +1,37 @@
 package com.example.androidfinalgroupproject;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The Dictionary class is the main activity that drives the dictionary application. This class
  * extends AppCompatActivity. Users have the options to search the dictionary for a word, view the
@@ -24,6 +41,12 @@ import android.widget.Toast;
  * @version 1.0.
  */
 public class Dictionary extends AppCompatActivity {
+
+    private ProgressBar pb;
+    private String searchTerm;
+
+
+
     /**
      * This method initializes the Dictionary activity. The setContentView() method is used to
      * define the layout resource to be used. Toolbar and ProgressBar are initialized, and a click
@@ -40,11 +63,30 @@ public class Dictionary extends AppCompatActivity {
         setToolbar();
         createProgressBar();
         Button saveButton = findViewById(R.id.saveButton);
+        Button searchButton = findViewById(R.id.searchButton);
+        EditText searchBar = findViewById(R.id.searchBar);
+
+        searchButton.setOnClickListener(btn -> {
+            View v = this.getCurrentFocus();
+            InputMethodManager mm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            mm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            try {
+                searchTerm = searchBar.getText().toString().trim();
+                String param = URLEncoder.encode(searchTerm, "UTF-8");
+                DataFetcher networkThread = new DataFetcher();
+                networkThread.execute("https://www.dictionaryapi.com/api/v1/references/sd3/xml/" + param
+                        + "?key=4556541c-b8ed-4674-9620-b6cba447184f");
+            }catch(Exception e){
+                Log.e("Program crashed", e.getMessage());
+            }
+        });
+
+
         saveButton.setOnClickListener(btn -> {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             LayoutInflater inflater = this.getLayoutInflater();
             LinearLayout l = findViewById(R.id.customDialog);
-            View v = inflater.inflate(R.layout.dictionary_custom_dialog, l);
+            View v = inflater.inflate(R.layout.activity_dictionary_dialog, l);
             builder.setView( v );
             builder.setPositiveButton(R.string.positive, (dialog, which) -> {
                 Intent nextPage = new Intent(Dictionary.this, DictionarySavedWords.class);
@@ -112,11 +154,105 @@ public class Dictionary extends AppCompatActivity {
      * Note: ProgressBar is set at 50% as a placeholder until full functionality is implemented.
      */
     private void createProgressBar(){
-        ProgressBar pb = findViewById(R.id.dictionaryProgress);
+        pb = findViewById(R.id.dictionaryProgress);
         pb.getProgressDrawable().setColorFilter(
                 getResources().getColor(R.color.colorAccentD),
                 android.graphics.PorterDuff.Mode.SRC_IN);
-        pb.setVisibility(View.VISIBLE);
-        pb.setProgress(50);
+        pb.setVisibility(View.INVISIBLE);
+    }
+
+    private class DataFetcher extends AsyncTask<String, Integer, String> {
+
+        List<String> headWord = new ArrayList<>();      //<hw>
+        List<String> pronunciation = new ArrayList<>(); //<pr>
+        List<String> wordType = new ArrayList<>();      //<fl>
+        List<String> inflections = new ArrayList<>();   //<if>
+        List<String> senseNumber = new ArrayList<>();   //<sn>
+        List<String> definition = new ArrayList<>();    //<dt>
+        List<String> references = new ArrayList<>();    //<sx>
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try{
+
+                String myUrl = strings[0];
+                URL url = new URL(myUrl);
+                HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+                urlConnection.connect();
+                InputStream i = urlConnection.getInputStream();
+                XmlPullParser pp = Xml.newPullParser();
+                pp.setInput(i, "UTF-8");
+                while(pp.getEventType() != XmlPullParser.END_DOCUMENT){
+                    if (pp.getEventType() == XmlPullParser.START_TAG){
+                        String tagName = pp.getName();
+                        switch (tagName) {
+                            case "hw":
+
+                                headWord.add(pp.nextText());
+                                publishProgress(10);
+
+                                break;
+                            case "pr":
+
+                                pronunciation.add('\\' + pp.nextText() + "\\");
+                                publishProgress(20);
+
+                                break;
+                            case "fl":
+
+                                wordType.add(pp.getAttributeValue(null, "fl"));
+                                publishProgress(30);
+
+                                break;
+                            case "if":
+
+                                inflections.add(pp.getAttributeValue(null, "if"));
+                                publishProgress(40);
+
+                                break;
+                            case "sn":
+
+                                senseNumber.add(pp.getAttributeValue(null, "sn"));
+                                publishProgress(60);
+
+                                break;
+                            case "dt":
+
+                                definition.add(pp.getAttributeValue(null, "dt"));
+                                publishProgress(75);
+                                references.add(pp.getAttributeValue(null, "sx"));
+                                publishProgress(100);
+
+                                break;
+                        }
+                    }
+                    pp.next();
+                }
+                i.close();
+
+            }catch(Exception e){
+                Log.e("Program crashed", e.getStackTrace().toString());
+            }
+            return "Finished Task";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            pb.setVisibility(View.INVISIBLE);
+            TextView hw = findViewById(R.id.headWord);
+            TextView pr = findViewById(R.id.pronunciation);
+
+            hw.setText(headWord.get(0));
+            pr.setText(pronunciation.get(0));
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            pb.setVisibility(View.VISIBLE);
+            pb.setProgress(values[0]);
+        }
     }
 }
